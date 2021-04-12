eb8f96db9815fd30ad97eab997f3a093616b4fbc
EliasYoc <eliyoc_97@hotmail.com>
2021-04-12 15:54:12 -0500

    correción de portfolio

--- js/color_cabecera.js
+++ js/color_cabecera.js
@@ -1,16 +1,20 @@
 const d = document,
-w = window;
+  w = window;
 
-export default function changeHeaderColor(){
-    const $header = d.getElementById("header");
-    w.addEventListener("scroll",(e)=>{
-        // console.log(w.pageYOffset)
-        if(w.matchMedia("(min-width: 930px)").matches){
-            (w.pageYOffset > 620) ? $header.style.backgroundColor = "var(--bg-second-light-mode)" : $header.style.backgroundColor = "transparent"
-        }
+export default function changeHeaderColor() {
+  const $header = d.getElementById("header");
+  w.addEventListener("scroll", (e) => {
+    // console.log(w.pageYOffset);
+    if (w.matchMedia("(min-width: 930px)").matches) {
+      w.pageYOffset > 573
+        ? ($header.style.backgroundColor = "var(--bg-second-light-mode)")
+        : ($header.style.backgroundColor = "transparent");
+    }
 
-        if(w.matchMedia("(max-width:930px)").matches){
-            (w.pageYOffset > 30) ? $header.style.backgroundColor = "var(--bg-second-light-mode)" : $header.style.backgroundColor = "transparent"
-        }
-    })
+    if (w.matchMedia("(max-width:930px)").matches) {
+      w.pageYOffset > 30
+        ? ($header.style.backgroundColor = "var(--bg-second-light-mode)")
+        : ($header.style.backgroundColor = "transparent");
+    }
+  });
 }
