f75debd92133580fb5d1bbaac6f1cd158f3b03d4
EliasYoc <eliyoc_97@hotmail.com>
2021-07-04 16:49:29 -0500

    mejora de animacion

--- js/color_cabecera.js
+++ js/color_cabecera.js
@@ -3,6 +3,9 @@ const d = document,
 
 export default function changeHeaderColor() {
   const $header = d.getElementById("header");
+  w.pageYOffset > 30
+    ? ($header.style.backgroundColor = "var(--bg-second)")
+    : ($header.style.backgroundColor = "transparent");
   w.addEventListener("scroll", (e) => {
     // console.log(w.pageYOffset);
     if (w.matchMedia("(min-width: 930px)").matches) {
--- js/dark-mode-switch.js
+++ js/dark-mode-switch.js
@@ -1,8 +1,21 @@
+const d = document;
 export default function toggleSwitch() {
+  const colorTheme = localStorage.getItem("theme-portfolio");
+
   document.addEventListener("click", (e) => {
     if (e.target.matches(".toggle-switch")) {
-      document.body.classList.toggle("light");
-      document.body.classList.toggle("dark");
+      d.body.classList.toggle("light");
+      d.body.classList.toggle("dark");
+      d.body.className === "dark"
+        ? localStorage.setItem("theme-portfolio", "dark")
+        : localStorage.setItem("theme-portfolio", "light");
     }
   });
+  if (colorTheme === "dark") {
+    d.body.classList.remove("light");
+    d.body.classList.add("dark");
+  } else {
+    d.body.classList.remove("dark");
+    d.body.classList.add("light");
+  }
 }
